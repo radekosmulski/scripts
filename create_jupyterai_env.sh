a3959addb95dec98cfe5e12815cc80039a5d3fbb
Radek Osmulski <rosmulski@gmail.com>
2025-01-12 11:03:17 +1000

    update

--- create_jupyterai_env.sh
+++ create_jupyterai_env.sh
@@ -1,5 +1,18 @@
+CONDA_PATH=$(conda info --base)
+source $CONDA_PATH/etc/profile.d/conda.sh
+conda deactivate
+
 # Remove existing jupyterai environment if it exists
-conda env remove -n jupyterai --yes
+if conda env list | grep -q "jupyterai"; then
+    echo "Found existing jupyterai environment. Removing..."
+    conda env remove -n jupyterai --yes
+fi
+
+# The env might have already been removed, but the directory might still exist
+if [ -d "${CONDA_PREFIX}/envs/jupyterai" ]; then
+    echo "Removing directory ${CONDA_PREFIX}/envs/jupyterai"
+    rm -rf "${CONDA_PREFIX}/envs/jupyterai"
+fi
 
 # Create new jupyterai environment with Python 3.12
 conda create -n jupyterai python=3.12 -y
@@ -10,6 +23,7 @@ pip install notebook
 pip install jupyter-ai-magics[all]
 
 # Prompt user about adding conda activation to .zshrc
+echo ""
 read -p "Would you like to add 'conda activate jupyterai' to your ~/.zshrc? (y/n) " answer
 if [[ $answer =~ ^[Yy]$ ]]; then
     echo -e "\n# Automatically activate jupyterai environment\nconda activate jupyterai" >> ~/.zshrc
